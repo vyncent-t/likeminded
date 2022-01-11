62d60c6b8e29bdc4045ad1818dec35d0c129c907
Vincent T <vyncode@gmail.com>
2022-01-11 16:47:42 -0500

    added npm bcrypt and jsonweb

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,24 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema(
     {
         username: {
             type: String,
             required: true,
+            trim: true,
             unique: true,
         },
         email: {
             type: String,
             required: true,
             unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!'],
         },
         password: {
             type: String,
             required: true,
+            minlength: 5,
         },
         created_cliques: {
             clique_id: [
@@ -58,6 +62,22 @@ const userSchema = new Schema(
         }
     }
 );
+
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
