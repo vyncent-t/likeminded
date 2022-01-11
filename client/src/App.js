ab219910418c642b9b53254d2eec259f6af41693
Vincent T <vyncode@gmail.com>
2022-01-11 15:38:19 -0500

    added redirect on path=* for router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import LandingPage from './pages/landingpage'
 import CreateLogInPage from './pages/createLogInPage';
 import Navbar from './components/Navbar';
@@ -15,12 +15,15 @@ const client = new ApolloClient({
 
 // replaces the switch route method of old
 
+// use navigate to act as the redirect for all other param inputs
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route path="/welcome" element={<CreateLogInPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </ApolloProvider>
   );
